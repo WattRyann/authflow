2e9418df96a3d7a1fe7dbe87ee6e425542bbb1a4
WattRyann <hua.zigzag@gmail.com>
2025-03-06 16:57:51 +1030

    Updated yarn.lock for Yarn 1.22.19

--- src/env.ts
+++ src/env.ts
@@ -5,10 +5,10 @@ config();
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
-  SECRET_KEY: z.string().min(32),
   JWT_ACCESS_SECRET: z.string().min(32),
   JWT_REFRESH_SECRET: z.string().min(32),
-  REFRESH_SECRET_KEY: z.string().min(32),
+  // SECRET_KEY: z.string().min(32),
+  // REFRESH_SECRET_KEY: z.string().min(32),
   ALGORITHM: z.enum(['HS256', 'HS384', 'HS512']),
   // 邮件配置
   MAIL_USERNAME: z.string().email(),
